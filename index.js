78d6ae4131c0999c02d449ef962e0c15e3831f83
Miriam Mae Iglesia <miriamiglesia@gmail.com>
2015-03-25 10:59:14 +0800

    testing token push

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 var Hapi = require( 'hapi');
 var server = new Hapi.Server();
-
+// testing token push
 server.connection( {
 	'port' : 8000
 } );
 
+server.route( [ {
+	'method' : 'GET',
+	'path' : '/v1/hello',
+	'handler' : function ( request, reply ) {
+		reply( 'hello one yami' );
+	}
+} ] );
 server.start( function onServerStart() {
 	console.log( 'starting server : ' + server.info.uri );
 });
